5859d4a3ad5ad319d457d9fbfc80fc1e2788308e
Yavuz TUNC <ytunc@borusan.com>
2021-07-23 02:40:06 +0300

    map added

--- Start/slice.go
+++ Start/slice.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func sliceSample() {
-	fmt.Println("Welcome to go")
+
+	s := make([]string, 3)
+	fmt.Println(s)
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+
+	fmt.Println("set: ", s)
+	fmt.Println("get: ", s[2])
+
+	fmt.Println("len: ", len(s))
+
+	s = append(s, "d", "e")
+	fmt.Println(s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("c: ", c)
+
 }
